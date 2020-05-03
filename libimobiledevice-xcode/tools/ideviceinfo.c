275d4c72992f8538c2f213dcab18cb136141ff62
xiao-long.liu <xiao-long.liu@microfocus.com>
2020-05-03 09:17:55 +0800

    init

---
+++ libimobiledevice-xcode/tools/ideviceinfo.c
@@ -0,0 +1,254 @@
+/*
+ * ideviceinfo.c
+ * Simple utility to show information about an attached device
+ *
+ * Copyright (c) 2010-2019 Nikias Bassen, All Rights Reserved.
+ * Copyright (c) 2009 Martin Szulecki All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <getopt.h>
+#ifndef WIN32
+#include <signal.h>
+#endif
+
+#include <libimobiledevice/libimobiledevice.h>
+#include <libimobiledevice/lockdown.h>
+#include "utils.h"
+
+#define FORMAT_KEY_VALUE 1
+#define FORMAT_XML 2
+
+static const char *domains[] = {
+	"com.apple.disk_usage",
+	"com.apple.disk_usage.factory",
+	"com.apple.mobile.battery",
+/* FIXME: For some reason lockdownd segfaults on this, works sometimes though
+	"com.apple.mobile.debug",. */
+	"com.apple.iqagent",
+	"com.apple.purplebuddy",
+	"com.apple.PurpleBuddy",
+	"com.apple.mobile.chaperone",
+	"com.apple.mobile.third_party_termination",
+	"com.apple.mobile.lockdownd",
+	"com.apple.mobile.lockdown_cache",
+	"com.apple.xcode.developerdomain",
+	"com.apple.international",
+	"com.apple.mobile.data_sync",
+	"com.apple.mobile.tethered_sync",
+	"com.apple.mobile.mobile_application_usage",
+	"com.apple.mobile.backup",
+	"com.apple.mobile.nikita",
+	"com.apple.mobile.restriction",
+	"com.apple.mobile.user_preferences",
+	"com.apple.mobile.sync_data_class",
+	"com.apple.mobile.software_behavior",
+	"com.apple.mobile.iTunes.SQLMusicLibraryPostProcessCommands",
+	"com.apple.mobile.iTunes.accessories",
+	"com.apple.mobile.internal", /**< iOS 4.0+ */
+	"com.apple.mobile.wireless_lockdown", /**< iOS 4.0+ */
+	"com.apple.fairplay",
+	"com.apple.iTunes",
+	"com.apple.mobile.iTunes.store",
+	"com.apple.mobile.iTunes",
+	NULL
+};
+
+static int is_domain_known(char *domain)
+{
+	int i = 0;
+	while (domains[i] != NULL) {
+		if (strstr(domain, domains[i++])) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+static void print_usage(int argc, char **argv, int is_error)
+{
+	int i = 0;
+	char *name = NULL;
+	name = strrchr(argv[0], '/');
+	fprintf(is_error ? stderr : stdout, "Usage: %s [OPTIONS]\n", (name ? name + 1: argv[0]));
+	fprintf(is_error ? stderr : stdout,
+	  "Show information about a connected device.\n\n" \
+	  "  -s, --simple       use a simple connection to avoid auto-pairing with the device\n" \
+	  "  -u, --udid UDID    target specific device by UDID\n" \
+	  "  -n, --network      connect to network device even if available via USB\n" \
+	  "  -q, --domain NAME  set domain of query to NAME. Default: None\n" \
+	  "  -k, --key NAME     only query key specified by NAME. Default: All keys.\n" \
+	  "  -x, --xml          output information as xml plist instead of key/value pairs\n" \
+	  "  -h, --help         prints usage information\n" \
+	  "  -d, --debug        enable communication debugging\n" \
+	  "\n"
+	);
+	fprintf(is_error ? stderr : stdout, "  Known domains are:\n\n");
+	while (domains[i] != NULL) {
+		fprintf(is_error ? stderr : stdout, "  %s\n", domains[i++]);
+	}
+	fprintf(is_error ? stderr : stdout,
+	  "\n" \
+//	  "Homepage: <" PACKAGE_URL ">\n"
+	);
+}
+
+int main(int argc, char *argv[])
+{
+	lockdownd_client_t client = NULL;
+	lockdownd_error_t ldret = LOCKDOWN_E_UNKNOWN_ERROR;
+	idevice_t device = NULL;
+	idevice_error_t ret = IDEVICE_E_UNKNOWN_ERROR;
+	int simple = 0;
+	int format = FORMAT_KEY_VALUE;
+	char* udid = NULL;
+	char *domain = NULL;
+	char *key = NULL;
+	char *xml_doc = NULL;
+	uint32_t xml_length;
+	plist_t node = NULL;
+	enum idevice_options lookup_opts = IDEVICE_LOOKUP_USBMUX | IDEVICE_LOOKUP_NETWORK;
+
+	int c = 0;
+	const struct option longopts[] = {
+		{ "debug", no_argument, NULL, 'd' },
+		{ "help", no_argument, NULL, 'h' },
+		{ "udid", required_argument, NULL, 'u' },
+		{ "network", no_argument, NULL, 'n' },
+		{ "domain", required_argument, NULL, 'q' },
+		{ "key", required_argument, NULL, 'k' },
+		{ "simple", no_argument, NULL, 's' },
+		{ "xml", no_argument, NULL, 'x' },
+		{ NULL, 0, NULL, 0}
+	};
+
+#ifndef WIN32
+	signal(SIGPIPE, SIG_IGN);
+#endif
+
+	while ((c = getopt_long(argc, argv, "dhu:nq:k:sx", longopts, NULL)) != -1) {
+		switch (c) {
+		case 'd':
+			idevice_set_debug_level(1);
+			break;
+		case 'u':
+			if (!*optarg) {
+				fprintf(stderr, "ERROR: UDID must not be empty!\n");
+				print_usage(argc, argv, 1);
+				return 2;
+			}
+			free(udid);
+			udid = strdup(optarg);
+			break;
+		case 'n':
+			lookup_opts |= IDEVICE_LOOKUP_PREFER_NETWORK;
+			break;
+		case 'q':
+			if (!*optarg) {
+				fprintf(stderr, "ERROR: 'domain' must not be empty!\n");
+				print_usage(argc, argv, 1);
+				return 2;
+			}
+			free(domain);
+			domain = strdup(optarg);
+			break;
+		case 'k':
+			if (!*optarg) {
+				fprintf(stderr, "ERROR: 'key' must not be empty!\n");
+				print_usage(argc, argv, 1);
+				return 2;
+			}
+			free(key);
+			key = strdup(optarg);
+			break;
+		case 'x':
+			format = FORMAT_XML;
+			break;
+		case 's':
+			simple = 1;
+			break;
+		case 'h':
+			print_usage(argc, argv, 0);
+			return 0;
+		default:
+			print_usage(argc, argv, 1);
+			return 2;
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	ret = idevice_new_with_options(&device, udid, lookup_opts);
+	if (ret != IDEVICE_E_SUCCESS) {
+		if (udid) {
+			printf("ERROR: Device %s not found!\n", udid);
+		} else {
+			printf("ERROR: No device found!\n");
+		}
+		return -1;
+	}
+
+	if (LOCKDOWN_E_SUCCESS != (ldret = simple ?
+			lockdownd_client_new(device, &client, "ideviceinfo"):
+			lockdownd_client_new_with_handshake(device, &client, "ideviceinfo"))) {
+		fprintf(stderr, "ERROR: Could not connect to lockdownd: %s (%d)\n", lockdownd_strerror(ldret), ldret);
+		idevice_free(device);
+		return -1;
+	}
+
+	if (domain && !is_domain_known(domain)) {
+		fprintf(stderr, "WARNING: Sending query with unknown domain \"%s\".\n", domain);
+	}
+
+	/* run query and output information */
+	if(lockdownd_get_value(client, domain, key, &node) == LOCKDOWN_E_SUCCESS) {
+		if (node) {
+			switch (format) {
+			case FORMAT_XML:
+				plist_to_xml(node, &xml_doc, &xml_length);
+				printf("%s", xml_doc);
+				free(xml_doc);
+				break;
+			case FORMAT_KEY_VALUE:
+				plist_print_to_stream(node, stdout);
+				break;
+			default:
+				if (key != NULL)
+					plist_print_to_stream(node, stdout);
+			break;
+			}
+			plist_free(node);
+			node = NULL;
+		}
+	}
+
+	if (domain != NULL)
+		free(domain);
+	lockdownd_client_free(client);
+	idevice_free(device);
+
+	return 0;
+}
+
